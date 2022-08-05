5f8db7b0038cf801f44254bea1cf46444a36652e
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2022-08-05 19:52:19 +0530

    Firstcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  window.open("https://wa.me/message/MQTSNEW4XD6MD1");
+}
+function ajsfun() {
+    var x = document.getElementById("drdw");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
