c91c6f252d2fe969400ba19e5b6d0b6a91ef2c4c
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2022-08-10 21:10:42 +0530

    draft update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  window.open("https://wa.me/message/MQTSNEW4XD6MD1");
+  window.open("https://wa.me/917907386458?text=Hello%20Simz%20Academy,%20I%20would%20like%20to%20know%20more%20about%20your%20courses.");
 }
 function ajsfun() {
     var x = document.getElementById("drdw");
